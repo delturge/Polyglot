20192d35eadd7ea4876f2d36315d8ad70b296bbf
delturge <anthony_rutledge_101@yahoo.com>
2020-03-23 07:46:27 -0400

    Create Degrees.py

---
+++ Python/Griddy/Degrees.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+# An enum holding minimum and maximum values for latitude and longitude.
+
+class Degrees(Enum):
+    MIN_LATITUDE = -90.0000000000000
+    MAX_LATITUDE = 90.0000000000000
+    MIN_LONGITUDE = -179.9999999999999
+    MAX_LONGITUDE = 180.0000000000000
